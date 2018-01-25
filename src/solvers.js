a9f1f82f7604bcac18274cd601539b3ea0a6358b
bstanger <beccastanger@gmail.com>
2018-01-24 20:09:09 -0800

    First attempt at rook logic

--- src/solvers.js
+++ src/solvers.js
@@ -14,9 +14,62 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-
+// assume n = 4
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // var solution;
+  // var maxIndex = n - 1;
+  // // Come up with all possible coordinate pairs [0,0][0,1]...[3,3] - 16
+
+  var possibleCols = []; // [0, 1, 2, 3]
+  console.log('n: ', n);
+  for (var j = 0; j < n; j++) {
+    possibleCols.push(j);
+  }
+
+  // for every n spots, make combo
+  // aiming for: [[0,1],[0,2],[2,3],[3,1]]
+  // remove possible row and column as go
+
+  var possibleValues = [0, 1];
+  var nItemCombo = [];
+  // Loop for each final coordinate
+  for (var i = 0; i < n; i++) {
+    var rowCombo = [];
+    
+    while (rowCombo.length < n) {
+      for (var k = 0; k < possibleValues.length; k++) { // possibleValues[k] = 0 or 1
+        if (possibleValues[k] === 1) {
+          rowCombo.push(possibleValues[k]); // [1]
+          while (rowCombo.length < n) {
+              rowCombo.push(0); // rowCombo = [1,0,0,0]
+          }
+        } else 
+        // [0] or [1]
+      }
+    }
+
+
+    for (var k = 0; k < possibleValues.length; k++) { // possibleValues[k] = 0 or 1
+      for (var m = 0; m < possibleCols.length; m++) { // possibleCols[m] = 0, 1, 2, 3
+        rowCombo.push(possibleValues[k])
+
+      //   coordinatePair.push(possibleRows[k], possibleCols[m]);
+      //   possibleRows.splice(k, 1); // [0, 1, 2]
+      //   possibleCols.splice(m, 1); // [1, 2, 3]
+      //   nItemCombo.push(coordinatePair);
+        // nItemCombo == [[0,1]]
+      }
+    }
+    // nItemCombo = [[0,1,0,0],[0,0,0,1],[0,0,1,0],[1,0,0,0]]
+  }
+  
+  debugger;
+
+  // remove duplicates
+  // {0:1,0:2,[2,3],[3,1]} and {[0,1],[0,2],[3,1],[2,3]}
+
+  // run tests to check for conflicts
+  
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
